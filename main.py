3db030790b0fd0a3207ffa58a0aa01ec3c8ead80
nest1code <nest1code@gmail.com>
2025-12-23 19:53:00 +0330

    creat file and cread first page

---
+++ main.py
@@ -0,0 +1,10 @@
+# import
+import turtle
+# set up the screen
+win=turtle.Screen()
+win.title('amir snake')
+win.bgcolor('green')
+win.setup(width=600,height=600)
+win.tracer(0)
+# keep the window open
+win.mainloop()
