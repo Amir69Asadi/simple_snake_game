bac54de3d2648f02956d9d4ffea57d413f7f7b8b
nest1code <nest1code@gmail.com>
2025-12-24 22:13:47 +0330

    add snake head

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ win.title('amir snake')
 win.bgcolor('green')
 win.setup(width=600,height=600)
 win.tracer(0)
-# keep the window open
+#snake head
+head=turtle.Turtle()
+head.speed()
+head.shape('circle')
+head.color('red')
+head.penup()
+head.goto(0,0)
+head.direction='stop'
+
+
+# main game loop
+while True:
+    win.update()
 win.mainloop()
